09f5393ead9959d076746b3e0b00b5dee5f5b2d9
Odin Hoff Gardå <odin.hoff.garda@proton.me>
2025-03-15 09:43:07 +0100

    small fixes to chatbot code

--- chatbot.py
+++ chatbot.py
@@ -40,32 +40,26 @@ with st.sidebar:
 if clear_chat:
     st.session_state.messages = []
 
+# Based on code from the Streamlit docs
+
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+        st.write(message["content"])
 
 # React to user input
 if prompt := st.chat_input("Type your question...", max_chars=100):
-    # Display user message in chat message container
-    st.chat_message("user").markdown(prompt)
-    # Add user message to chat history
+    st.chat_message("user").write(prompt)
     st.session_state.messages.append({"role": "user", "content": prompt})
 
-    # Generate assistant response
+    # Sample answer from model 
     response = sample_sequence(model, tokenizer, prompt, strategy, config.max_len, config.device, p=top_p, temperature=temperature) 
 
-    print(f"Prompt: {prompt}\nResponse: {response}\n")
-
-    # Display assistant response in chat message container
-    with st.chat_message("assistant"):
-        st.write(response) # or st.write(response)
-    # Add assistant response to chat history
+    st.chat_message("assistant").write(response)
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 
